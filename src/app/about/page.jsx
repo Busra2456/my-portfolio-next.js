68229cf949ca8528458ddf27739b4b3aa1ee5f53
Busra2456 <you@example.com>
2026-02-15 16:15:06 +0600

    9time

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -6,39 +6,26 @@ export const metadata = {
   keywords: ["about","about page"]
 };
 
-// const getTime = async ()=>{
-//       const res = await 
-//      fetch("http://localhost:3000/time",
-//             // {cache: 'no-store'}
-//             {next: {revalidate : 5}}
+const getTime = async ()=>{
+      const res = await 
+     fetch("http://localhost:3000/time",
+            {cache: 'no-store'}
+            // {next: {revalidate : 5}}
 
-//       );
-//       const data = await res.json()
-//       console.log(data)
-//       return data.currentTime ;
-// }
+      );
+      const data = await res.json()
+      console.log(data)
+      return data.currentTime ;
+}
 
-// export const revalidate = 5;
 
 const AboutPage = async () => {
-        const res = await fetch(`${process.env.PUBLIC_API_URL}/time`, {
-    cache: "no-store", 
-  });
-  const data = await res.json();
+        
 
-//       const res = await 
-//      fetch("http://localhost:3000/time",
-//             // {cache: 'no-store'}
-//             {next: {revalidate : 5}}
-
-//       );
-//       const data = await res.json()
-
-      // const currentTime = await getTime();
-      const currentTime = new Date().toLocaleTimeString();
+      const currentTime = await getTime();
       return (
             <div>
-                  <h3>Time : {data.currentTime} </h3>
+                  <h3>Time : {currentTime} </h3>
                   <h1>about page</h1>
                   <AboutHeader></AboutHeader>
             </div>
