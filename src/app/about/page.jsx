213a3ca240c6ceac654022d5626e1ccf5933743c
Busra2456 <you@example.com>
2026-02-15 14:11:11 +0600

    time4

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,4 +1,4 @@
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 import AboutHeader from '@/Components/AboutHeader';
 export const metadata = {
   title: "About",
