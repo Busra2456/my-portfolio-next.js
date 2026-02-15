25f08b46e3986865dcfb7cea2f5657ddf543c5ba
Busra2456 <you@example.com>
2026-02-15 16:17:30 +0600

    10time

--- src/app/time/route.js
+++ src/app/time/route.js
@@ -1,16 +1,16 @@
-// export async function GET(){
-//       return Response.json({
-//             currentTime : new Date().toLocaleTimeString()
-//       })
+// // export async function GET(){
+// //       return Response.json({
+// //             currentTime : new Date().toLocaleTimeString()
+// //       })
+// // }
+// export const dynamic = "force-static";
+// export const revalidate = 5;
+
+
+// export async function GET() {
+//   const currentTime = new Date().toLocaleTimeString();
+//   return new Response(JSON.stringify({ currentTime }), {
+//     status: 200,
+//     headers: { "Content-Type": "application/json" },
+//   });
 // }
-export const dynamic = "force-static";
-export const revalidate = 5;
-
-
-export async function GET() {
-  const currentTime = new Date().toLocaleTimeString();
-  return new Response(JSON.stringify({ currentTime }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
-}
