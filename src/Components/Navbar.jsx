6de6e4376149893165f3a096a9d7b30265a76719
Busra2456 <you@example.com>
2026-02-23 14:38:24 +0600

    Set up sign-in with email and password

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 "use client"
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import Image from "next/image";
@@ -46,41 +46,61 @@ const Navbar = () => {
                   title : "Gallery",
                   path : '/gallery'
             },
+               {
+                  title : "dashboard",
+                  path : '/dashboard'
+            },
       ];
       const handler = () =>{
-            router.push('/api/auth/signin')
+            router.push('/api/auth/signin'
+            )
       }
 
       if(pathName.includes("dashboard"))
-            return(
+       return (
      
       <div className="bg-amber-400 p-6">
             dashboard Layout
-      </div>
-      )
+      </div>)
+      
       return (
             <nav className="bg-red-400 pr-10 pl-3 py-4 flex justify-between items-center text-xl">
-        <h6 className="text-3xl">My <span className="text-red-800">Portfolio</span></h6>
-          <ul className="flex justify-between items-center text-xl space-x-10 ">
+        <h6 className="text-3xl">
+            <Link href={"/"}>
+            My <span className="text-red-800">Portfolio</span>
+            </Link>
+            </h6>
+          <ul className="flex justify-between items-center text-xl space-x-8 ">
             {/* <li><Link href={'/about'}>About</Link></li>
             <li><Link href={'/service'}>Service</Link></li>
             <li><Link href={'/contact'}>Contact</Link></li> */}
             {
-                  links?.map((link) =><Link className={`${pathName === link.path && "text-fuchsia-800"}`} key={link.path} href={link.path}>{link.title}</Link>)
+                  links?.map((link) =><Link className={`${pathName === link.path && "text-fuchsia-800 "}`} key={link.path} href={link.path}>{link.title}</Link>)
             }
           </ul>
+         
+         
        
-         {session.status === "authenticated" ? <button onClick={handler}>Login</button> : <button onClick={handler}>Logout</button>
+         {session.status !== "authenticated" ?( <button
+        onClick={handler}
+        >
+          Login
+        </button>
+        ) : (
+            <button  onClick={() =>signOut()}>Logout</button> )
          }
-         <div>
-            <h6>
-                  <Image height={50} width={50} alt={session?.data?.user?.name} src={session?.data?.user?.image}/>
+        
+        
+            <div className="">
+                  <Image height={30} width={30} className="rounded-xl" alt={session?.data?.user?.name}  src={session?.data?.user?.image }/>
                   <br />
                   {session?.data?.user?.name}
                   <br />
                    {session?.data?.user?.type}
-            </h6>
-         </div>
+            </div>
+        
+         
+          
         </nav>
       );
 };
--- src/middleware.js
+++ src/middleware.js
@@ -1,18 +1,24 @@
+// import { cookies } from "next/headers"
 import {NextResponse } from "next/server"
-const user = true;
-const coo = 'next-superhero'
+// const user = true;
+
 export const middleware = (request) =>{
-const cookies = request.cookies.get('token')
+      const token = request.cookies.get("next-auth.session-token")
+      console.log(token)
+// const cookies = request.cookies.get('token')
 // if(!user){
 //       return NextResponse.redirect(new URL('/login',request.url))
 // }
-if(!cookies || cookies.value !== coo){
-      return NextResponse.redirect(new URL('/login',request.url))
+// if(!cookies || cookies.value !== coo){
+//       return NextResponse.redirect(new URL('/login',request.url))
+// }
+if( !token){
+      return NextResponse.redirect(new URL('/api/auth/signin',request.url))
 }
 return NextResponse.next();
      
 }
 
 export const config = {
-      matcher :['/dashboard/:path*',]
+      matcher :['/dashboard/:path*','/service/:path*']
 }
